56d48862b6778a832222abdf1da581cf4d7d77a4
dekrt <dekrt@qq.com>
2026-03-16 11:33:14 +0800

    feat: update epochs and add temporal weight to projection loss

--- jit/scripts/train.sh
+++ jit/scripts/train.sh
@@ -7,8 +7,7 @@ torchrun --nproc_per_node=2 main_jit.py --config configs/irepa.yaml \
   --enc_type="dinov3-vit-b16" \
   --encoder_depth=4 \
   --data_path=../data \
-  --epochs 1000 \
-  --max_train_steps 100 \
+  --epochs 200 \
   --output_dir="/lpai/output/models/jit-dinov3-vit-b16-irepa" \
   --report_to tensorboard \
 
