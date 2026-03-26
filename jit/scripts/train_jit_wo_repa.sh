a9095009591003c4d61606070ddfc7bddf37c08d
dekrt <dekrt@qq.com>
2026-03-26 14:26:52 +0800

    feat: update training script to adjust CUDA devices and batch size

--- jit/scripts/train_jit_wo_repa.sh
+++ jit/scripts/train_jit_wo_repa.sh
@@ -11,7 +11,7 @@ cd /lpai/volumes/so-volume-bd-ga/lhp/code/zxk/iREPA-dev/jit
 mkdir -p /root/.cache/torch/hub/checkpoints/
 cp /lpai/volumes/so-volume-bd-ga/lhp/pt_inception-2015-12-05-6726825d.pth /root/.cache/torch/hub/checkpoints/weights-inception-2015-12-05-6726825d.pth
 
-CUDA_VISIBLE_DEVICES=6,7 \
+CUDA_VISIBLE_DEVICES=4,6 \
 torchrun --nproc_per_node=2 main_jit.py \
   --model JiT-B/16 \
   --enc_type="dinov3-vit-b16" \
@@ -19,6 +19,7 @@ torchrun --nproc_per_node=2 main_jit.py \
   --data_path=../data \
   --epochs 200 \
   --output_dir="/lpai/output/models/jit-dinov3-vit-b16-jit-wo-repa" \
-  --report_to tensorboard
+  --report_to tensorboard \
+  --batch_size 16
 
 sleep 1d
