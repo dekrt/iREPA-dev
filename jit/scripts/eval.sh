fe763f6accd5a0f4e78dd94aaad548ba89685a8b
dekrt <dekrt@qq.com>
2026-04-03 14:53:47 +0800

    feat: update evaluation script with new output and checkpoint directory paths

--- jit/scripts/eval.sh
+++ jit/scripts/eval.sh
@@ -1,16 +1,16 @@
-OUTPUT_DIR="/lpai/output/models/iREPA/eval/JiT_repa-semantic-nce-wo-spnorm"
-CKPT_DIR="/lpai/models/repa/jit-dinov3-vit-b16-irepa-semantic-nce-wo-spnorm/jit-dinov3-vit-b16-irepa-semantic-nce-wo-spnorm/checkpoint-100.pth"
+OUTPUT_DIR="/lpai/output/models/iREPA/eval/JiT_fft_freq-asym-mse"
+CKPT_DIR="/lpai/models/repa/jit-dinov3-vit-b16-repa-vanilla-nce/jit-dinov3-vit-b16-repa-vanilla_nce/checkpoint-100.pth"
 
 mkdir -p /root/.cache/torch/hub/checkpoints/
 cp /lpai/volumes/so-volume-bd-ga/lhp/pt_inception-2015-12-05-6726825d.pth /root/.cache/torch/hub/checkpoints/weights-inception-2015-12-05-6726825d.pth
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 \
+CUDA_VISIBLE_DEVICES=3,4,5,7 \
 torchrun --nproc_per_node=4 --nnodes=1 --node_rank=0 \
     --master_port 29600 \
     main_jit.py \
     --model JiT-B/16 \
     --img_size 256 --noise_scale 1.0 \
-    --gen_bsz 2048 --num_images 50000 --cfg 3.0 --interval_min 0.1 --interval_max 1.0 \
+    --gen_bsz 1024 --num_images 50000 --cfg 3.0 --interval_min 0.1 --interval_max 1.0 \
     --output_dir ${OUTPUT_DIR} \
     --resume ${CKPT_DIR} \
     --report_to tensorboard \
