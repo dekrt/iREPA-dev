457d0f952b7805acf150932d01cab24773102edd
dekrt <dekrt@qq.com>
2026-04-03 15:58:35 +0800

    feat: update training script to change CUDA devices and modify output directory and projection loss type

--- jit/scripts/train_repa.sh
+++ jit/scripts/train_repa.sh
@@ -10,15 +10,15 @@ cd /lpai/volumes/so-volume-bd-ga/lhp/code/zxk/iREPA-dev/jit
 mkdir -p /root/.cache/torch/hub/checkpoints/
 cp /lpai/volumes/so-volume-bd-ga/lhp/pt_inception-2015-12-05-6726825d.pth /root/.cache/torch/hub/checkpoints/weights-inception-2015-12-05-6726825d.pth
 
-CUDA_VISIBLE_DEVICES=6,7 \
+CUDA_VISIBLE_DEVICES=1,2 \
 torchrun --nproc_per_node=2 main_jit.py --config configs/repa.yaml \
   --model JiT-B/16 \
   --enc_type="dinov3-vit-b16" \
   --encoder_depth=4 \
   --data_path=../data \
   --epochs 200 \
-  --output_dir="/lpai/output/models/jit-dinov3-vit-b16-repa-cosine" \
+  --output_dir="/lpai/output/models/jit-dinov3-vit-b16-repa-global_anchor_cosine" \
   --report_to tensorboard \
-  --projection_loss_type cosine
+  --projection_loss_type global_anchor_cosine
 
 sleep 1d
